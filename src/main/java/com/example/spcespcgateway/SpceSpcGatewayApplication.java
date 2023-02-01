176911cf2a0c5649877de2953d34474b80e3b2f9
ImKunYoung <ur2kunyoung2@outlook.com>
2023-02-01 23:44:49 +0900

    initial commit

---
+++ src/main/java/com/example/spcespcgateway/SpceSpcGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.spcespcgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpceSpcGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpceSpcGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spcespcgateway/SpceSpcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spcespcgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpceSpcGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
